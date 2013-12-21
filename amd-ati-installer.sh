f76630dbe22d3e5cff1bc92c2213b1725040e2a4
ConanHUN <conan_513@hotmail.com>
2013-12-21 13:10:08 +0100

    Update amd-ati-installer.sh

--- amd-ati-installer.sh
+++ amd-ati-installer.sh
@@ -4,7 +4,7 @@ sudo apt-get install lib32gcc1 &&
 mkdir ~/Catalyst && mkdir ~/Catalyst/13.12 &&
 cd ~/Catalyst/13.12 &&
 
-wget http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip &&
+wget --referer='http://support.amd.com/en-us/download/desktop?os=Linux+x86' http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip &&
 unzip amd-catalyst-13.12-linux-x86.x86_64.zip &&
 chmod +x amd-catalyst-13.12-linux-x86.x86_64.run &&
 sudo sh ./amd-catalyst-13.12-linux-x86.x86_64.run --buildpkg Ubuntu/saucy &&
