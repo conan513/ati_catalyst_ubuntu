f19f8998ca032fe500cd544e6e4d682ee6e973a6
ConanHUN <conan_513@hotmail.com>
2013-12-21 12:56:04 +0100

    Update amd-ati-installer.sh

--- amd-ati-installer.sh
+++ amd-ati-installer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-sudo apt-get install build-essential cdbs dh-make dkms execstack dh-modaliases linux-headers-generic &&
+sudo apt-get install cdbs dh-make dkms execstack dh-modaliases linux-headers-generic libqtgui4 &&
 sudo apt-get install lib32gcc1 &&
-mkdir ~/Catalyst && mkdir ~/Catalyst/13.2b7 &&
-cd ~/Catalyst/13.2b7 &&
+mkdir ~/Catalyst && mkdir ~/Catalyst/13.12 &&
+cd ~/Catalyst/13.12 &&
 
-wget http://www2.ati.com/drivers/beta/amd-driver-installer-catalyst-13.2-beta7-linux-x86.x86_64.zip &&
-unzip amd-driver-installer-catalyst-13.2-beta7-linux-x86.x86_64.zip &&
-chmod +x amd-driver-installer-catalyst-13.2-beta7-linux-x86.x86_64.run &&
-sudo sh ./amd-driver-installer-catalyst-13.2-beta7-linux-x86.x86_64.run --buildpkg Ubuntu/quantal &&
+wget http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip &&
+unzip amd-catalyst-13.12-linux-x86.x86_64.zip &&
+chmod +x amd-catalyst-13.12-linux-x86.x86_64.run &&
+sudo sh ./amd-catalyst-13.12-linux-x86.x86_64.run --buildpkg Ubuntu/saucy &&
 sudo dpkg -i fglrx*.deb &&
 sudo aticonfig --initial -f &&
 
